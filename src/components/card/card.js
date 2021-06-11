3be1b196bcba288ae36c6262bc0e5459c0c6014f
Allen <alleneben@gmail.com>
2021-06-11 15:19:21 +0000

    new changes to incoorprate form

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -6,7 +6,7 @@ class Card extends React.Component {
 
     render(){
         return(
-            <div className={this.props.className} style={{opacity: this.props.submittingstyle}} id={this.props.id}>
+            <div className={this.props.className} {...this.props}>
                 {this.props.children}
             </div>
         )
